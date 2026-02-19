9e48f5e629aaa8d9b98b759b647ed2d36adfde97
John <johneliud4@gmail.com>
2026-02-19 09:28:00 +0300

    feat: add JSON serialization annotations to Role enum. This update enhances the Role enum with Jackson annotations to ensure proper deserialization from string values and consistent serialization output when the enum is used in API request and response payloads.

--- src/main/java/io/github/johneliud/user_service/models/Role.java
+++ src/main/java/io/github/johneliud/user_service/models/Role.java
@@ -1,6 +1,19 @@
 package io.github.johneliud.user_service.models;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum Role {
     CLIENT,
-    SELLER
+    SELLER;
+
+    @JsonCreator
+    public static Role fromString(String value) {
+        return Role.valueOf(value.toUpperCase());
+    }
+
+    @JsonValue
+    public String toValue() {
+        return name();
+    }
 }
