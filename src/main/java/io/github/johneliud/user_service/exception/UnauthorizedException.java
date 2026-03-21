8e1ddca17f53dbfbba7ef6d26a328dc408f58f22
John <johneliud4@gmail.com>
2026-03-21 17:27:38 +0300

    feat: create custom UnauthorizedException for user-service. This update adds a custom exception class for authentication errors.

---
+++ src/main/java/io/github/johneliud/user_service/exception/UnauthorizedException.java
@@ -0,0 +1,7 @@
+package io.github.johneliud.user_service.exception;
+
+public class UnauthorizedException extends RuntimeException {
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
