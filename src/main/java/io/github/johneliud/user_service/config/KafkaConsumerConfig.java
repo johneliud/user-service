5f79e7c8f96a40bf08124c69312d45f40197070e
John <johneliud4@gmail.com>
2026-03-23 16:52:42 +0300

    chore(user-service): add default values to Kafka config properties for local development fallback.

--- src/main/java/io/github/johneliud/user_service/config/KafkaConsumerConfig.java
+++ src/main/java/io/github/johneliud/user_service/config/KafkaConsumerConfig.java
@@ -17,10 +17,10 @@ import java.util.Map;
 @Configuration
 public class KafkaConsumerConfig {
 
-    @Value("${spring.kafka.bootstrap-servers}")
+    @Value("${spring.kafka.bootstrap-servers:localhost:9092}")
     private String bootstrapServers;
 
-    @Value("${spring.kafka.consumer.group-id}")
+    @Value("${spring.kafka.consumer.group-id:user-service}")
     private String groupId;
 
     @Bean
